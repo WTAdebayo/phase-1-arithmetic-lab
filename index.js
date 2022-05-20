f81c625badf501161f54a77ba4b45dd4a8037d69
Adewole Adebayo <wole1105@gmail.com>
2022-05-20 01:36:21 -0700

    commit tree 030585132db7451ea5d1aabe6c493382f0c75c71

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+
+const multiply = num1 * num2;
+console.log(multiply);
+
+const random = Math.ceil(Math.random());
+
+
+const num3 = 64
+const num4 = 10
+const mod = num3 % num4;
+
+const max = Math.max(4, 8, 12, 16, 20);
